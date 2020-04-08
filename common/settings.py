3c20cd9680264a08b4e5b9dd724350eb65f1dd62
Shaad <shaad1@live.nl>
2020-04-08 16:30:28 +0200

    Refactor settings module to use a parse function so we can easily handle exceptions

--- common/settings.py
+++ common/settings.py
@@ -6,26 +6,47 @@ from pathlib import Path
 '''
 Contains the required settings for the python scripts from the yaml along with some pre-processing
 '''
+_config = {}
+def reload():
+    with open("../settings.yaml", "r") as file:
+        global _config
+        _config = yaml.safe_load(file.read())
+reload()
 
-with open("../settings.yaml", "r") as file:
-    config = yaml.safe_load(file.read())
+class ConfigParseError(Exception):
+    pass
 
-base_sheet_path = Path(config["base_sheet_path"]) # Can just use the "/" operator with Path
-mongo_address = re.search('[a-zA-Z]+:[0-9]+', config["mongo_server"]).group(0).split(":") # [ip, port]
-rabbitmq_address = config["rabbitmq_address"].split(":") # [ip, port] 
-score_queue_name = config["mq_score_queue"]
-sheet_queue_name = config["mq_sheet_queue"]
-new_item_queue_name = config["mq_new_item_queue"]
-sheet_collection_name = config["mongo_sheet_collection"]
-score_collection_name = config["mongo_score_collection"]
-slice_collection_name = config["mongo_slice_collection"]
-aggregated_result_collection_name = config["mongo_aggregated_result_collection"]
-score_rebuilder_queue_name = config["mq_score_rebuilder_queue"]
-github_user = config["github_user"]
-github_token = config["github_token"]
-github_organization_name = config["github_organization"]
-github_queue_name = config["mq_github_queue"]
-github_init_queue_name = config["mq_github_init_queue"]
-github_branch_name = config["github_branch"]
-github_commit_count_before_push = int(config["github_commit_count_before_push"])
-task_collection_name = config["mongo_task_collection"]
+# "lenient" is a temporary solution to make debugging easier.
+# Long-term solution: only import settings that are needed at the locations they are used --> need to turn them into lamdbas
+def _parse(entry, f=lambda x : x, default="", lenient=True):
+    try:
+        entry_type = type(default)
+        return entry_type(f(_config[entry])) # Use constructor of entry type
+    except Exception as e:
+        if lenient:
+            print(f"WARNING: Could not parse config entry '{entry}', ensure it has the correct format, continuing with default value: {default}")
+        else:
+            raise ConfigParseError(f"ERROR: Could not parse config entry '{entry}', ensure it has the correct format.", e)
+    return default
+
+
+
+base_sheet_path                     = _parse("base_sheet_path", default=Path("sheets"))
+mongo_address                       = _parse("mongo_server", f=lambda x : re.search('[a-zA-Z]+:[0-9]+', x).group(0).split(":")) 
+rabbitmq_address                    = _parse("rabbitmq_address", f=lambda x: x.split(":")) # [ip, port] 
+score_queue_name                    = _parse("mq_score_queue")
+sheet_queue_name                    = _parse("mq_sheet_queue")
+new_item_queue_name                 = _parse("mq_new_item_queue")
+sheet_collection_name               = _parse("mongo_sheet_collection")
+score_collection_name               = _parse("mongo_score_collection")
+slice_collection_name               = _parse("mongo_slice_collection")
+aggregated_result_collection_name   = _parse("mongo_aggregated_result_collection")
+score_rebuilder_queue_name          = _parse("mq_score_rebuilder_queue")
+github_user                         = _parse("github_user")
+github_token                        = _parse("github_token")
+github_organization_name            = _parse("github_organization")
+github_queue_name                   = _parse("mq_github_queue")
+github_init_queue_name              = _parse("mq_github_init_queue")
+github_branch_name                  = _parse("github_branch")
+github_commit_count_before_push     = _parse("github_commit_count_before_push", default=5)
+task_collection_name                = _parse("mongo_task_collection")
