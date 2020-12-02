61a2ab9f17de83148c0fd11d772609b9d1055991
Archer6621 <shaad1@live.nl>
2020-12-02 14:59:29 +0100

    Update MEI format for title in img measure detector as well

--- pdf_to_mei/measure_detector/folder_to_mei_imgmd.py
+++ pdf_to_mei/measure_detector/folder_to_mei_imgmd.py
@@ -17,7 +17,9 @@ def run(sheet_name):
     <mei xmlns="http://www.music-encoding.org/ns/mei">
         <meiHead>
             <fileDesc>
-                <titleStmt/>
+                <titleStmt>
+                    <title/>
+                </titleStmt>
                 <pubStmt/>
             </fileDesc>
              <encodingDesc>
