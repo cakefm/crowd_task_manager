bb6548af1281f00a40a00866b08946c2201f2a8d
Archer6621 <shaad1@live.nl>
2020-12-03 22:23:38 +0100

    Use tree aligner for score rebuilding, with small workaround

--- score_rebuilder/score_rebuilder_mq.py
+++ score_rebuilder/score_rebuilder_mq.py
@@ -6,12 +6,13 @@ sys.path.append("..")
 from common.settings import cfg
 import common.file_system_manager as fsm
 import common.tree_tools as tt
+import common.tree_alignment as ta
+
 import xml.dom.minidom as xml
 from bson.objectid import ObjectId
 
 from pymongo import MongoClient
 
-# Score rebuilder should always re-index, and rely on the measure order instead of the n./label-attribute
 def callback(ch, method, properties, body):
     data = json.loads(body)
     sheet_name = data['name']
@@ -22,57 +23,37 @@ def callback(ch, method, properties, body):
 
     # Get MEI file and measures
     mei_path = fsm.get_sheet_whole_directory(sheet_name) / "aligned.mei"
-    mei_xml = xml.parse(str(mei_path))
-    mei_measures = mei_xml.getElementsByTagName("measure")
+    mei_xml_tree = xml.parse(str(mei_path))
+    # mei_measures = mei_xml.getElementsByTagName("measure")
 
     # Obtain corresponding task and slice
     task = db[cfg.col_task].find_one({"_id" : ObjectId(task_id)})
-    measure_staff_slice = db[cfg.col_slice].find_one({"_id" : ObjectId(task["slice_id"])})
-    slice_measures = mei_measures[measure_staff_slice["start"]: measure_staff_slice["end"]]
+    # measure_staff_slice = db[cfg.col_slice].find_one({"_id" : ObjectId(task["slice_id"])})
+    # slice_measures = mei_measures[measure_staff_slice["start"]: measure_staff_slice["end"]]
 
     # Get aggregated XML
     aggregated_result = db[cfg.col_aggregated_result].find_one({"task_id" : task_id, "step": task["step"]})
-    aggregated_xml = xml.parseString(aggregated_result["result"]).documentElement
-    aggregated_measures = aggregated_xml.getElementsByTagName("measure")
+    aggregated_xml = aggregated_result["result"]
 
-    # Apply the changes
+    # Temporary solution: give the slice somewhat more context by inserting only the header of the previous measure into it
+    tree = xml.parseString(aggregated_xml).documentElement
+    index = int(tree.getElementsByTagName("measure")[0].getAttribute("n")) - 1  # n-index is shifted up by 1
+    if index > 0:
+        measure = mei_xml_tree.getElementsByTagName("measure")[index - 1].cloneNode(deep=True)  # get the previous measure
+        measure.childNodes = []
+        tree.insertBefore(measure, tree.childNodes[0])
+        aggregated_xml = tree.toxml()
 
-    # Perform all skeletal modifications in stage 0
-    if task["stage"]==0:
-        # Adjust measure skeleton
-        if len(aggregated_measures) > len(slice_measures):
-            for measure in aggregated_measures[len(slice_measures):]:
-                mei_measures[0].parentNode.insertBefore(measure, slice_measures[-1].nextSibling)
-
-        # TODO: Adjust staff skeleton
-        # ...
-
-        # TODO: Adjust facsimile values
-        # ...
-
-        # Re-enumerate
-        for index, measure in enumerate(mei_measures):
-            n = index + 1
-            measure.setAttribute("n", str(n))
-            measure.setAttribute("label", str(n))
-
-            for staff_index, staff in enumerate(measure.getElementsByTagName("staff")):
-                staff_n = staff_index + 1
-                staff.setAttribute("n", str(n))
-
-
-
-    # Perform non-skeletal modifications in any other stage
-    else:
-        for measure_original, measure_agg in zip(slice_measures, aggregated_measures):
-            staffs_original = measure_original.getElementsByTagName("staff")[measure_staff_slice["staff_start"]: measure_staff_slice["staff_end"]]
-            staffs_aggregated = measure_agg.getElementsByTagName("staff")
-            for staff_original, staff_aggregated in zip(staffs_original, staffs_aggregated):
-                tt.replace_child_nodes(staff_original, staff_aggregated.childNodes)
+    # Perform combination with original MEI via tree aligner
+    mei_section = mei_xml_tree.getElementsByTagName("section")[0]
+    mei_section_xml = mei_section.toxml()
+    aligned_trees = ta.align_trees_multiple([mei_section_xml, aggregated_xml], distance_function=ta.node_distance_anchored)
+    final_section_tree, _ = ta.build_consensus_tree(aligned_trees, consensus_method=ta.consensus_bnd_enrich_skeleton)
+    tt.replace_child_nodes(mei_section, final_section_tree.childNodes)
 
     # Write MEI file
     with open(str(mei_path), 'w') as mei_file:
-        mei_file.write(tt.purge_non_element_nodes(mei_xml.documentElement).toprettyxml())
+        mei_file.write(tt.purge_non_element_nodes(mei_xml_tree.documentElement).toprettyxml())
 
     status_update_msg = {
     '_id': task_id,
