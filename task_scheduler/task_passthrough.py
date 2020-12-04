42f87929cd38cdf3cf9f9aa8e3cf56f4475c8be0
Archer6621 <shaad1@live.nl>
2020-12-04 18:26:19 +0100

    Add support for note editing in task result simulator

--- task_scheduler/task_passthrough.py
+++ task_scheduler/task_passthrough.py
@@ -18,15 +18,19 @@ from pymongo import MongoClient
 key_modes = ["minor", "major"]
 key_sigs = ["3f", "2f", "1f"]
 
+# <note dur="4" oct="4" pname="a"/>
+note_dur = ["4", "8"]
+note_oct = ["4", "5", "6"]
+note_pname = ["a", "b", "c", "d", "e", "f", "g"]
+
 # Fractions
 scoredef_chance = 0.9
 clef_chance = 0.1
+note_chance = 0.1
 
 # Which staff/measure indices to process
 indices = {}
 staff_indices = {}
-
-
 
 
 def populate_payload_xml(score, tree, task):
@@ -35,11 +39,13 @@ def populate_payload_xml(score, tree, task):
     measures = tree.getElementsByTagName("measure")
     first_measure_index = measures[0].getAttribute("n")
 
+    # Scoredefs
     if task_type_name=="0_fix_scoredef" and scoredef_chance > rnd.random():
         node = tt.create_element_node("scoreDef", {"key.sig": rnd.choice(key_sigs), "key.mode": rnd.choice(key_modes), "crmp_id": str(uuid.uuid4())})
         print(f"  Putting scoreDef {node.toxml()} before measure n={first_measure_index}")
         tree.insertBefore(node, measures[0]).parentNode
 
+    # Clef detection
     elif task_type_name=="1_detect_clefs" and task_step_name=="edit" and clef_chance > rnd.random():
         node = tt.create_element_node("clef", {"shape":"G", "line":"2"})
         staff = tree.getElementsByTagName("staff")[0]
@@ -49,6 +55,21 @@ def populate_payload_xml(score, tree, task):
         for layer in staff.getElementsByTagName("layer"):
             layer.appendChild(node.cloneNode(deep=True))
             break
+
+    # Note editing
+    elif task_type_name=="2_add_notes" and task_step_name=="edit":
+        staff = tree.getElementsByTagName("staff")[0]
+        staff_index = staff.getAttribute('n')
+        for layer in staff.getElementsByTagName("layer"):
+            for i in [_ for _ in range(4) if note_chance > rnd.random()]:
+                note_attr = {
+                    "dur": rnd.choice(note_dur),
+                    "oct": rnd.choice(note_oct),
+                    "pname": rnd.choice(note_pname)
+                }
+                node = tt.create_element_node("note", note_attr)
+                print(f"  Putting note {node.toxml()} in measure n={first_measure_index}, staff n={staff_index}")
+                layer.appendChild(node.cloneNode(deep=True))
 
     return tt.purge_non_element_nodes(tree).toxml()
 
