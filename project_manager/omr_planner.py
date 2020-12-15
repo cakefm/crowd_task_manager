fa1cc35478a799b7d8e5678730fe2e4103bb4816
Archer6621 <shaad1@live.nl>
2020-12-15 02:05:23 +0100

    Only send to task scheduler when both github init and slicer had their first status report

--- project_manager/omr_planner.py
+++ project_manager/omr_planner.py
@@ -7,32 +7,21 @@ import functools
 sys.path.append("..")
 from common.settings import cfg
 import common.file_system_manager as fsm
-from collections import namedtuple
 
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 
 
-def schedule_message(trigger, message, queue):
-    print(f"Scheduling message {message} on trigger {trigger} for queue {queue}")
-    scheduled_messages[trigger] = (message, queue)
+def check_dependencies(score_name, modules):
+    return all([(score_name, m) in ready_dependencies for m in modules])
 
-
-def check_scheduled_messages(trigger, channel):
-    if trigger in scheduled_messages:
-        message, queue = scheduled_messages.pop(trigger)
-        send_message(
-            message,
-            queue,
-            channel
-        )
-
+def update_dependency(score_name, module):
+    ready_dependencies.add((score_name, module))
 
 def send_message(message, queue, channel):
     json_str = json.dumps(message)
     channel.queue_declare(queue=queue)
     channel.basic_publish(exchange='', routing_key=queue, body=json_str)
-
 
 # TODO: ack implementation is a bit rudimentary for now, in the long term we would like
 #       to only ack when the work was completed succesfully. For this we need to store
@@ -57,9 +46,10 @@ def take_action_on_status(channel, method, properties, body):
     score_status = json.loads(body)
     module = score_status['module']
     score_name = score_status['name']
+    update_dependency(score_name, module)
     delivery_tag = method.delivery_tag
     ack = functools.partial(ack_message, channel, delivery_tag)
-    check_scheduled_messages((module, score_name), channel)
+    # check_scheduled_messages((module, score_name), channel)
     if module == 'measure_detector':
         # Probably a good time to create a campaign-status object,
         # only insert campaign status if it doesn't exist already though
@@ -146,19 +136,20 @@ def take_action_on_status(channel, method, properties, body):
         if campaign_status["finished"]:
             print(f"Campaign for {score_name} has finished, no need to do more tasks")
         else:
-            print(
-                datetime.now(),
-                'sending ',
-                score_status['name'], 'task_scheduler')
-            send_message(
-                {
-                    '_id': score_status['_id'],
-                    'name': score_status['name'],
-                    'action': 'start_next_stage'
-                },
-                cfg.mq_task_scheduler,
-                channel
-            )
+            if check_dependencies(score_name, {'slicer', 'github_init'}):
+                print(
+                    datetime.now(),
+                    'sending ',
+                    score_status['name'], 'task_scheduler')
+                send_message(
+                    {
+                        '_id': score_status['_id'],
+                        'name': score_status['name'],
+                        'action': 'start_next_stage'
+                    },
+                    cfg.mq_task_scheduler,
+                    channel
+                )
         ack()
     elif module == 'task_scheduler' and 'stage' in score_status:
         # Task scheduler will always mention stage
@@ -184,6 +175,27 @@ def take_action_on_status(channel, method, properties, body):
         print(f"Completed {task_type} for score {score_name}")
         ack()
     elif module == 'github_init':
+        url = f"https://github.com/{cfg.github_organization}/{score_name}/tree/{cfg.github_branch}"
+        # Temporary solution until something better can be found
+        # TODO: We need a way to deal with waiting for multiple messages, possibly using the same
+        #       delivery-tag method that proper acks will use
+        
+        # Depending on which of the two finishes sooner, one of them will only send
+        if check_dependencies(score_name, {'slicer', 'github_init'}):
+            print(
+                datetime.now(),
+                'sending ',
+                score_status['name'], 'task_scheduler')
+            send_message(
+                {
+                    '_id': score_status['_id'],
+                    'name': score_status['name'],
+                    'action': 'start_next_stage'
+                },
+                cfg.mq_task_scheduler,
+                channel
+            )
+
         # communicate to ce that github repo has been initialized
         ack()
 
@@ -226,7 +238,7 @@ if __name__ == "__main__":
         queue=cfg.mq_omr_planner_status
     )
 
-    scheduled_messages = {}
+    ready_dependencies = set()
 
     print('OMRP is listening...')
     channel.start_consuming()
