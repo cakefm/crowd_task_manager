2efe1fe9d38a398ec3d84eed35a6bc3d298e2e73
Shaad Alaka <shaad1@.live.nl>
2021-03-24 18:03:16 +0100

    Remove logs folder on start-up, and ensure live config is present

--- start_local.sh
+++ start_local.sh
@@ -1,3 +1,7 @@
 export TROMPA_BACKEND_MODE=TEST
 export TROMPA_API_PORT=8888
+if [ ! -f settings_docker_live.yaml ]; then
+    cp settings_docker_test.yaml settings_docker_live.yaml
+fi
+rm -R ./logs
 docker-compose down --volumes && docker-compose build --force-rm trompa && docker-compose up
