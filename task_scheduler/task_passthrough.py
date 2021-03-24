387ea21152e283cc291fc335930fba375857e90a
Shaad Alaka <shaad1@.live.nl>
2021-03-24 18:02:19 +0100

    Rework task pass-through module to work with new tasks

--- task_scheduler/task_passthrough.py
+++ task_scheduler/task_passthrough.py
@@ -14,104 +14,172 @@ import uuid
 
 from pymongo import MongoClient
 
-# Choices
-key_modes = ["minor", "major"]
-key_sigs = ["3f", "2f", "1f"]
+error_chance = 0.0
 
-# <note dur="4" oct="4" pname="a"/>
-note_dur = ["4", "8"]
-note_oct = ["4", "5", "6"]
-note_pname = ["a", "b", "c", "d", "e", "f", "g"]
+task_set = set()
 
-# Fractions
-scoredef_chance = 0.9
-clef_chance = 0.1
-note_chance = 0.1
+def first_or_none(tree, tag, condition = None):
+    elements = tree.getElementsByTagName(tag)
+    if condition:
+        elements = [e for e in elements if condition(e)]
+    return None if not len(elements) else elements[0]
 
-# Which staff/measure indices to process
-indices = {}
-staff_indices = {}
+def set_attributes(node, attributes):
+    for attr in attributes:
+        node.setAttribute(attr, attributes[attr])
 
-def populate_payload_xml(score, tree, task):
-    task_type_name = task["type"]
-    task_step_name = task["step"]
-    measures = tree.getElementsByTagName("measure")
-    first_measure_index = measures[0].getAttribute("n")
+def randomize_clef(tree):
+    clef = tt.create_element_node("clef")
+    choice = rnd.choice([
+        {"shape": "G", "line": "2"},
+        {"shape": "F", "line": "4"},
+        {"shape": "C", "line": "3"},
+        {}
+    ])
+    set_attributes(clef, choice)
+    first_or_none(tree, "layer").appendChild(clef)
+    return tree.toxml()
 
-    # Scoredefs
-    if task_type_name=="0_fix_scoredef" and scoredef_chance > rnd.random():
-        node = tt.create_element_node("scoreDef", {"key.sig": rnd.choice(key_sigs), "key.mode": rnd.choice(key_modes), "crmp_id": str(uuid.uuid4())})
-        print(f"  Putting scoreDef {node.toxml()} before measure n={first_measure_index}")
-        tree.insertBefore(node, measures[0]).parentNode
+def randomize_key(tree):
+    measure = first_or_none(tree, "measure")
+    n = first_or_none(tree, "staff").getAttribute("n")
+    scoreDef = first_or_none(tree, "scoreDef")
+    if not scoreDef:
+        scoreDef = tt.create_element_node("scoreDef")
+        tree.insertBefore(scoreDef, measure)
 
-    # Clef detection
-    elif task_type_name=="1_detect_clefs" and task_step_name=="edit" and clef_chance > rnd.random():
-        node = tt.create_element_node("clef", {"shape":"G", "line":"2"})
-        staff = tree.getElementsByTagName("staff")[0]
-        first_staff_index = staff.getAttribute('n')
-        print(f"  Putting clef {node.toxml()} in measure n={first_measure_index}, staff n={first_staff_index}")
+    staffGrp = first_or_none(scoreDef, "staffGrp")
+    if not staffGrp:
+        staffGrp = tt.create_element_node("staffGrp")
+        scoreDef.appendChild(staffGrp)
 
-        for layer in staff.getElementsByTagName("layer"):
-            layer.appendChild(node.cloneNode(deep=True))
-            break
+    staffDef = first_or_none(staffGrp, "staffDef", lambda e: e.getAttribute("n") == n)
+    if not staffDef:
+        staffDef = tt.create_element_node("staffDef", {"n": n})
+        staffGrp.appendChild(staffDef)
+    
+    choice = rnd.choice([
+        {"key.sig.show": "true", "key.sig": f"{rnd.randint(5)}f"},
+        {"key.sig.show": "true", "key.sig": f"{rnd.randint(5)}s"},
+        {"key.sig.show": "true", "key.sig": f"{rnd.randint(5)}0"},
+        {}
+    ])
 
-    # Note editing
-    elif task_type_name=="2_add_notes" and task_step_name=="edit":
-        staff = tree.getElementsByTagName("staff")[0]
-        staff_index = staff.getAttribute('n')
-        for layer in staff.getElementsByTagName("layer"):
-            for i in [_ for _ in range(4) if note_chance > rnd.random()]:
-                note_attr = {
-                    "dur": rnd.choice(note_dur),
-                    "oct": rnd.choice(note_oct),
-                    "pname": rnd.choice(note_pname)
-                }
-                node = tt.create_element_node("note", note_attr)
-                print(f"  Putting note {node.toxml()} in measure n={first_measure_index}, staff n={staff_index}")
-                layer.appendChild(node.cloneNode(deep=True))
+    set_attributes(staffDef, choice)
 
-    return tt.purge_non_element_nodes(tree).toxml()
+    return tree.toxml()
 
 
-def populate_payload_form(score, tree, task, data):
-    task_type_name = task["type"]
-    task_step_name = task["step"]
+def randomize_time(tree):
+    measure = first_or_none(tree, "measure")
+    n = first_or_none(tree, "staff").getAttribute("n")
+    scoreDef = first_or_none(tree, "scoreDef")
+    if not scoreDef:
+        scoreDef = tt.create_element_node("scoreDef")
+        tree.insertBefore(scoreDef, measure)
 
-    if task_type_name=="1_detect_clefs" and task_step_name=="verify":
-        data["verify"] = [rnd.random() < 0.8]
+    staffGrp = first_or_none(scoreDef, "staffGrp")
+    if not staffGrp:
+        staffGrp = tt.create_element_node("staffGrp")
+        scoreDef.appendChild(staffGrp)
 
-    return json.dumps(data)
+    staffDef = first_or_none(staffGrp, "staffDef", lambda e: e.getAttribute("n") == n)
+    if not staffDef:
+        staffDef = tt.create_element_node("staffDef", {"n": n})
+        staffGrp.appendChild(staffDef)
+    
+    choice = rnd.choice([
+        {"meter.sym": "true"},
+        {"meter.count": f"{1 + rnd.randint(4)}", "meter.unit": f"{1 + rnd.randint(4)}"},
+        {}
+    ])
+
+    set_attributes(staffDef, choice)
+
+    return tree.toxml() 
+
+def randomize_note(tree):
+    note_count = rnd.randint(8)
+    layer = first_or_none(tree, "layer")
+    for i in range(note_count):
+        element = tt.create_element_node(
+            rnd.choice(["note", "rest"]), 
+            {"dur": rnd.choice(['1/32', '1/16', '1/8', '1/4', '1/2', '1'])}
+        )
+        layer.appendChild(element)
+    
+
+    return tree.toxml()
+
+def randomize_pitch(tree):
+    notes = tree.getElementsByTagName("note")
+
+    for note in notes:
+        set_attributes(note, 
+            {
+            "pname":f"{rnd.choice(['a', 'b', 'c', 'd', 'e', 'f', 'g'])}", 
+            "oct":f"{3 + rnd.randint(3)}"
+            }
+        )
+
+    return tree.toxml()
+
 
 def callback(channel, method, properties, body):
-
     message = json.loads(body)
+    if 'action' in message and message['action'] != 'task created':
+        print(f"Ignoring message {message}...")
+        return
     task_id = message["task_id"]
+
+    if task_id in task_set:
+        print(f"[Warning] Got an additional creation request for task {task_id}")
+    task_set.add(task_id)
+
     task = db[cfg.col_task].find_one({"_id": ObjectId(task_id)})
-    score = db[cfg.col_score].find_one({"name": task["score"]})
-    task_type_name = task["type"]
+    task_type_name = message['task_type']
+    
+    # score = db[cfg.col_score].find_one({"name": task["score"]})
     task_type = db[cfg.col_task_type].find_one({"name": task_type_name})
     task_step_name = task["step"]
+
+    if task_step_name == "DONE":
+        print("[Warning] Received creation request for a task that was done already, skipping...")
+        return
+
     threshold = task_type["steps"][task_step_name]["min_responses"]
-    result_type = task_type["steps"][task_step_name]["result_type"]
+    # result_type = task_type["steps"][task_step_name]["result_type"]
 
     tree = xml.parseString(task['xml']).documentElement
 
-    print(f"Processing task {task_id} of type {task_type} at step {task_step_name}; providing result type {result_type}")
+    payload_func = {
+        "0_clef_recognition": randomize_clef,
+        "1_time_recognition": randomize_time,
+        "2_key_recognition": randomize_key,
+        "3_note_transcription": randomize_note,
+        "4_note_pitch": randomize_pitch
+    }[task_type_name]
 
-    if result_type=="xml":
-        payload = populate_payload_xml(score, tree, task)
-    elif result_type=="form":
-        payload = populate_payload_form(score, tree, task, {})
+    payload = payload_func(tree)
 
-    # TODO: Somehow vary the responses per submission
+    # print(f"Processing task {task_id} of type {task_type} at step {task_step_name}; providing result type {result_type}")
+
+    # if result_type=="xml":
+    #     payload = populate_payload_xml(score, tree, task)
+    # elif result_type=="form":
+    #     payload = populate_payload_form(score, tree, task, {})
+
     for i in range(threshold):
+        if (error_chance > rnd.random()):
+            payload = payload_func(tree)
         requests.post(f"http://localhost:443/{task['_id']}", data=payload)
-        for j in range(0): # Putting this to >0 will simulate delay in response
+        print(f"Sending response to API for task {task_id} (count {1 + i})")
+        for j in range(6): # Putting this to >0 will simulate delay in response
             connection.process_data_events()
-            time.sleep(0.2)
+            time.sleep(0.1 + 0.1 * rnd.random())
 
-    print(f"  Passed through task with ID {task['_id']} of type:step {task['type']}:{task['step']} as result {threshold} times")
-    print(f"  - payload: {payload}")
+    # print(f"  Passed through task with ID {task['_id']} of type:step {task['type']}:{task['step']} as result {threshold} times")
+    # print(f"  - payload: {payload}")
 
 
 if __name__ == "__main__":
@@ -123,10 +191,10 @@ if __name__ == "__main__":
     parameters = pika.ConnectionParameters(*cfg.rabbitmq_address)
     connection = pika.BlockingConnection(parameters)
     channel = connection.channel()
-    channel.queue_declare(queue=cfg.mq_task_passthrough)
+    channel.queue_declare(queue=cfg.mq_ce_communicator)
     channel.basic_consume(
         on_message_callback=callback, 
-        queue=cfg.mq_task_passthrough, 
+        queue=cfg.mq_ce_communicator, 
         auto_ack=True
     )
 
